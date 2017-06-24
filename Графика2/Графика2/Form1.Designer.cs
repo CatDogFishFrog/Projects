6a9be8a596407c081e5e3c5523d3ea1dd2a6abdb
VadimDadim <vadim.goncharov.1999@gmail.com>
2017-06-24 23:42:52 +0300

    Графика!!! Там короче торты, эллипсы рисует и всё такое

---
+++ Графика2/Графика2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Графика2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DrawBezier - Кривая Безье",
+            "DrawCurve - Кривая",
+            "DrawClosedCurve - Замкнутая кривая",
+            "DrawRectangle - Прямоугольник",
+            "DrawPolygon - Многоугольник",
+            "DrawEllipse - Эллипс",
+            "DrawArc - Дуга эллипса",
+            "DrawPie - Торт"});
+            this.comboBox1.Location = new System.Drawing.Point(177, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Dash - Тире",
+            "DashDot - Тире + точка",
+            "DashDotDot Тире + 2 точки",
+            "Dot - Точка"});
+            this.comboBox2.Location = new System.Drawing.Point(446, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(190, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "FillClosedCurve - Замкнутая кривая",
+            "FillRectangle - Прямоугольник",
+            "FillPolygon - Многоугольник",
+            "FillEllipse - Эллипс",
+            "FillPie - Торт"});
+            this.comboBox3.Location = new System.Drawing.Point(177, 110);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(239, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Cross - Сетка",
+            "DiagonalCross - Диагональная сетка",
+            "ForwardDiagonal - Диагональная прямая",
+            "BackwardDiagonal - Обратная диагональная прямая"});
+            this.comboBox4.Location = new System.Drawing.Point(446, 110);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(190, 21);
+            this.comboBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выбрать фигуру";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Выбрать стиль линей";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Выбрать фигуру с заливкой";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(443, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Выбрать стиль заливки";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Adobe Caslon Pro", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(177, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(459, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 246);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Графика2/Графика2/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Графика2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(this.Handle); //где рисуем
+            Pen pen = new Pen(Color.Black);//чем рисуем
+            Brush brush = Brushes.White;
+            Brush brush1 = Brushes.Blue;
+            HatchBrush brush2 = new HatchBrush(HatchStyle.Cross, ForeColor, BackColor); //Определяет заливку в виде сетки
+            HatchBrush brush3 = new HatchBrush(HatchStyle.DiagonalCross, ForeColor, BackColor); //Определяет заливку в виде диагональной сетки
+            HatchBrush brush4 = new HatchBrush(HatchStyle.ForwardDiagonal, ForeColor, BackColor); //Определяет заливку в виде диагональной прямой
+            HatchBrush brush5 = new HatchBrush(HatchStyle.BackwardDiagonal, ForeColor, BackColor); //Определяет заливку в виде обратной диагонали
+
+            Point[] p = //ну это наверное определяется местоположение первого рисунка
+            {
+                new Point(10,0),
+                new Point(80,70),
+                new Point(90,50),
+                new Point(50,90),
+            };
+
+            Point[] p1 = //а это, я думаю, местоположение второго рисунка
+            {
+                new Point(0,110),
+                new Point(80,180),
+                new Point(90,160),
+                new Point(50,200),
+            };
+            Rectangle rect = new Rectangle(0, 0, 110, 220);
+            Rectangle rect1 = new Rectangle(10, 0, 10, 100); //а вот теперь я уже совсем не понимаю что где определяет
+            Rectangle rect2= new Rectangle(10, 110, 100, 100);
+
+            g.FillRectangle(brush, rect);
+
+            if (comboBox2.SelectedIndex == 0) //начало выбора стилей линии
+            {
+                pen.DashStyle = DashStyle.Dash;
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+                pen.DashStyle = DashStyle.DashDot;
+            }
+            if (comboBox2.SelectedIndex == 2)
+            {
+                pen.DashStyle = DashStyle.DashDotDot;
+            }
+            if (comboBox2.SelectedIndex == 3)
+            {
+                pen.DashStyle = DashStyle.Dot;
+            }                                 //конец выбора стилей линии
+
+
+            if (comboBox4.SelectedIndex == 0) //начало выбора стиля заливки
+            {
+                brush1 = brush2;
+            }
+            if (comboBox4.SelectedIndex == 1)
+            {
+                brush1 = brush3;
+            }
+            if (comboBox4.SelectedIndex == 2)
+            {
+                brush1 = brush4;
+            }
+            if (comboBox4.SelectedIndex == 3)
+            {
+                brush1 = brush5;
+            }                                 // конец выбора стиля заливки
+
+
+            if (comboBox1.SelectedIndex == 3)   //3 DrawRectangle
+            {
+                g.DrawRectangle(pen, rect1);
+            }
+            if (comboBox3.SelectedIndex == 1)   //1 FillRectangle
+            {
+                g.FillRectangle(brush1,10,110,100,100);
+            }
+            if (comboBox1.SelectedIndex == 4)   //4 DrawPolygon
+            {
+                g.DrawPolygon(pen, p);
+            }
+            if (comboBox3.SelectedIndex == 2) //2 FillPolygon
+            {
+                g.FillPolygon(brush1, p1);
+            }
+            if (comboBox1.SelectedIndex == 5)  //5 DrawEllipse
+            {
+                g.DrawEllipse(pen,rect1);
+            }
+            if (comboBox3.SelectedIndex == 3) //3 FillEllipse
+            {
+                g.FillEllipse(brush1,10,110,100,100);
+            }
+            if (comboBox1.SelectedIndex == 7) //7 DrawPie
+            {
+                g.DrawPie(pen, rect1,50,250);
+            }
+            if (comboBox3.SelectedIndex == 4) //4 FillPie
+            {
+                g.FillPie(brush1, rect1, 50, 250);
+            }
+            if (comboBox1.SelectedIndex == 1) //1 DrawCurve
+            {
+                g.DrawCurve(pen, p);
+            }
+            if (comboBox1.SelectedIndex == 2) //2 DrawClosedCurve
+            {
+                g.DrawClosedCurve(pen, p);
+            }
+            if (comboBox3.SelectedIndex == 0) //0 FillClosedCurve
+            {
+                g.FillClosedCurve(brush1, p1);
+            }
+            if (comboBox1.SelectedIndex == 6) //6 DrawArc
+            {
+                g.DrawArc(pen, rect1,50,250);
+            }
+            if (comboBox1.SelectedIndex == 0) //0 DrawBezier
+            {
+                g.DrawBezier(pen, p[0],p[1],p[2],p[3]);
+            }
+        }
+    }
+}
