198b88c1627955c91b402bac0a24b18e70c2a528
Вадим Гончаров <vadim.goncharov.1999@outlook.com>
2017-09-17 14:47:57 +0300

    Кажется это змейка

---
+++ Змейка C++/Snake/game.cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+const int width = 20;
+const int height = 20;
+int tailX[100], tailY[100];
+int nTail;
+bool gameOver;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+int dif;
+
+void Setup() {
+	gameOver = false;
+	cout << "Select difficulty\n(1 - Very Easy \\ 2 - Easy \\ 3 - Normal \\ 4 - Hard)" << endl;
+	r:
+	switch (_getch())
+	{
+	case '1':
+		dif = 1000;
+		break;
+	case '2':
+		dif = 500;
+		break;
+	case '3':
+		dif = 250;
+		break;
+	case '4':
+		dif = 100;
+		break;
+	default:
+		goto r;
+	}
+
+	dir = STOP;
+	x = width / 2 - 1;
+	y = height / 2 - 1;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score - 0;
+
+}
+
+void Draw() {
+	system("cls");
+	for (int i = 0; i < width+1; i++)
+		cout <<" #";
+	cout << endl;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if(j==0 || j==width-1)
+				cout << " #";
+			if (i == y && j == x)
+				cout << " 0";
+			else if (i == fruitY && j == fruitX)
+				cout<<" *";
+			else {
+				bool print = false;
+				for (int k = 0; k < nTail; k++) {
+					if (tailX[k] == j && tailY[k] == i) {
+						print = true;
+						cout << " o";
+					}
+				}
+				if(!print)
+					cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < width + 1; i++)
+	{
+		cout << " #";
+	}
+	cout << endl;
+	cout << "Score: " << score << endl;
+}
+
+void Input() {
+	
+	if (_kbhit()) {
+		switch (_getch() )
+		{
+		case 'a':
+			if(dir!=RIGHT)
+				dir = LEFT;
+			break;
+		case 'd':
+			if(dir!=LEFT)
+				dir = RIGHT;
+			break;
+		case 'w':
+			if(dir!=DOWN)
+				dir =	UP;
+			break;
+		case 's':
+			if(dir!=UP)
+				dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
+}
+
+void Logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+
+	switch (dir)
+	{
+	
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	}
+	if (x > width-2 || x<0 || y>height || y < 0)
+		gameOver = true;
+
+	for (int i = 0; i < nTail; i++) {
+		if (tailX[i] == x&&tailY[i] == y) {
+			gameOver = true;
+		}
+	}
+
+	if (x == fruitX&&y == fruitY) {
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		nTail++;
+	}
+}
+
+int main() {
+	l:
+	Setup();
+	while (!gameOver)
+	{
+		Sleep(dif);
+		Draw();
+		Input();
+		Logic();
+	}
+	cout << "Game Ower!"<<endl;
+	cout << "Do you want to start the game again? (Y//N)"<<endl;
+	e:
+	switch (_getch())
+	{
+	case 'y':
+		goto l;
+		break;
+	case 'n':
+		return 0;
+		break;
+	default:
+		goto e;
+
+	}
+	return 0;
+}
---
+++ Калькулятор/Calculator/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Components;
+using MetroFramework.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : MetroForm
+    {
+        bool plus = false;
+        bool minus = false;
+        bool gam = false;
+        bool gay = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) { textBox1.Text += 1; }
+
+        private void button2_Click(object sender, EventArgs e) { textBox1.Text += 2; }
+
+        private void button3_Click(object sender, EventArgs e) { textBox1.Text += 3; }
+
+        private void button4_Click(object sender, EventArgs e) { textBox1.Text += 4; }
+
+        private void button5_Click(object sender, EventArgs e) { textBox1.Text += 5; }
+
+        private void button6_Click(object sender, EventArgs e) { textBox1.Text += 6; }
+
+        private void button7_Click(object sender, EventArgs e) { textBox1.Text += 7; }
+
+        private void button8_Click(object sender, EventArgs e) { textBox1.Text += 8; }
+
+        private void button9_Click(object sender, EventArgs e) { textBox1.Text += 9; }
+
+        private void button10_Click(object sender, EventArgs e) { textBox1.Text += 0; }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") { textBox1.Text = Convert.ToString(Math.Sqrt(Convert.ToDouble(textBox1.Text))); }
+            else return;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") { textBox1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox1.Text)); }
+            else return;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                plus = true;
+                textBox1.Tag = textBox1.Text;
+                textBox1.Text = "";
+            }
+            else return;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                minus = true;
+                textBox1.Tag = textBox1.Text;
+                textBox1.Text = "";
+            }
+            else return;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                gam = true;
+                textBox1.Tag = textBox1.Text;
+                textBox1.Text = "";
+            }
+            else return;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                gay = true;
+                textBox1.Tag = textBox1.Text;
+                textBox1.Text = "";
+            }
+            else return;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (plus)
+            {
+                decimal x = Convert.ToDecimal(textBox1.Tag) + Convert.ToDecimal(textBox1.Text);
+                textBox1.Text = x.ToString();
+                plus = false;
+            }
+            if (minus)
+            {
+                decimal x = Convert.ToDecimal(textBox1.Tag) - Convert.ToDecimal(textBox1.Text);
+                textBox1.Text = x.ToString();
+                minus = false;
+            }
+            if (gam)
+            {
+                decimal x = Convert.ToDecimal(textBox1.Tag) * Convert.ToDecimal(textBox1.Text);
+                textBox1.Text = x.ToString();
+                gam = false;
+            }
+            if (gay)
+            {
+                decimal x = Convert.ToDecimal(textBox1.Tag) / Convert.ToDecimal(textBox1.Text);
+                textBox1.Text = x.ToString();
+                gay = false;
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            plus = minus = gay = gam = false;
+            textBox1.Text = "";
+            textBox1.Tag = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains(",")) { return; }
+            else { textBox1.Text = textBox1.Text + ","; }
+        }
+    }
+}
